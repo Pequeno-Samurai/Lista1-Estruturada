baf9d376d233f035f1f14f693aa1851837890918
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-17 10:43:47 -0300

    Update Exercicio3-Estruturada.c

--- Exercicio3-Estruturada.c
+++ Exercicio3-Estruturada.c
@@ -10,47 +10,44 @@ int main(void)
 	printf("Digite um nome: ");
 	scanf("%[^\n]s", nome);
   
- 
 	for(int i = 0; i<40;i++)
 	{
     	if (i == 0)
-		{
+	{
       	auxiliar[a] = toupper(nome[0]);
       	a++;
-    }
-    if (nome[i] == 32)
+    	}
+    	if (nome[i] == 32)
 	{
-      auxiliar[a] = toupper(nome[i + 1]);
-      a++;
-      }
-    if (nome[i] == 0 && b == 0)
+        auxiliar[a] = toupper(nome[i + 1]);
+        a++;
+        }
+    	if (nome[i] == 0 && b == 0)
 	{
-      for(int x = strlen(nome)-1;x>0;x--)
-	  {
+        for(int x = strlen(nome)-1;x>0;x--)
+	{
         if(nome[x] == 32)
-		{
-          break;
-          }
+	{
+        break;
+        }
         else
-		{
-          sobrenome[b] = toupper(nome[x]);
-          b++;
+	{
+        sobrenome[b] = toupper(nome[x]);
+        b++;
         }
       }
     }
-    
   }
-  
   auxiliar[a] = 0;
   
 	for(int i = strlen(sobrenome); i>=0; i--)
-  {
-    printf("%c",sobrenome[i]);
-    }
+  	{
+    	printf("%c",sobrenome[i]);
+    	}
 	printf(", ");
 	for(int i = 0;i < strlen(auxiliar) -1;i++)
 	{
-    printf("%c.",auxiliar[i]);
-  }
+    	printf("%c.",auxiliar[i]);
+  	}
   return 0;
 }
